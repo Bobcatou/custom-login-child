382911e733aba22d2305f22465ca704558a24cba
Bryan Cady <sw@listentothewindmedia.com>
2015-12-05 13:38:14 -0500

    Create functions.php
    Add to functions.php

---
+++ functions.php
@@ -0,0 +1,28 @@
+/**
+*Custom Login Logo
+**/
+function my_loginlogo() {
+  echo '<style type="text/css">
+    h1 a {
+      background-image: url(' . get_stylesheet_directory_uri() . '/images/logo.png) !important;
+    }
+  </style>';
+}
+/**
+*Hover Title for Logo
+**/
+add_action('login_head', 'my_loginlogo');
+
+function my_loginURLtext() {
+    return 'Plan Curtail';
+}
+add_filter('login_headertitle', 'my_loginURLtext');
+
+/**
+*Enquere Login Custom Stylesheet
+**/
+function my_logincustomCSSfile() {
+    wp_enqueue_style('login-styles', get_stylesheet_directory_uri() . '/login_styles.css');
+}
+add_action('login_enqueue_scripts', 'my_logincustomCSSfile');
+
